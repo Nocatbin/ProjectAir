4ec978845c2552ce35c417b5b851ad0051889f36
Nocatbin <1500586815@qq.com>
2020-09-20 16:45:19 +0800

    增加了TFTLCD显示模块

--- USER/main.c
+++ USER/main.c
@@ -5,21 +5,26 @@
 #include "usart.h"
 #include "I2C.h"
 #include "SCD30.h"
+#include "lcd.h"
 
 
 int main(void)
 {
 	u8 data[18]={0};
 	u8 ready = 0;
+	u32 count = 1;
 	float CO2, Temperature, Humidity;
 	
 	delay_init();	    	 //延时函数初始化	  
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	uart_init(115200);	 //串口初始化为
+	LCD_Init();			   	//初始化LCD
 	I2C_congfig();
 	I2C_StretchClockCmd(I2C2, ENABLE);
 	SCD30_TriggerContinuousMeasurement();
 	delay_ms(3);
+	POINT_COLOR=BLUE;//设置字体为蓝色
+	LCD_ShowString(60,50,300,24,24, "Stability Test In Progress");
 	while(1)
 	{
 		ready = SCD30_CheckDataReady();
@@ -32,6 +37,15 @@ int main(void)
 		printf("Co2:%f", CO2);
 		printf("T:%f", Temperature);
 		printf("H:%f\n", Humidity);
+		LCD_ShowString(60,80,200,24,24, "CO2");
+		LCD_ShowNum(60,110,CO2,5,24);
+		LCD_ShowString(60,140,200,24,24, "Temp");
+		LCD_ShowNum(60,170,Temperature,2,24);
+		LCD_ShowString(60,200,200,24,24, "Humidity");
+		LCD_ShowNum(60,230,Humidity,2,24);
+		LCD_ShowString(60,260,200,24,24, "Count");
+		LCD_ShowNum(60,290,count,2,24);
+		count++;
 		}
 	}
 }
